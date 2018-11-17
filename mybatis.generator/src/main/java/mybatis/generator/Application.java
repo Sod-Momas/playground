67e91b6f24ab269c2d9e620334195c34e1e670b0
Sod-Momas <sothereer@gmail.com>
2018-11-17 10:35:50 +0800

    add mysql generator

--- mybatis.generator/src/main/java/mybatis/generator/Application.java
+++ mybatis.generator/src/main/java/mybatis/generator/Application.java
@@ -8,33 +8,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.mybatis.generator.api.MyBatisGenerator;
-import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.*;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * 启动类
+ */
 public class Application {
-	public static void main(String[] args) {
-		try {
-			gen();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
-	private static void gen()
-			throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
-		List<String> warnings = new ArrayList<String>();
-		boolean overwrite = true;
-		InputStream configFile = Application.class.getClassLoader().getResourceAsStream("generatorConfig.xml");
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final String CONFIG_FILE = "generatorConfig-mysql.xml";
+
+    public static void main(String[] args) {
+        try {
+            gen();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void gen() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
 //		File configFile = new File("generatorConfig.xml");
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = cp.parseConfiguration(configFile);
-		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-		myBatisGenerator.generate(null);
+        InputStream configFile = Application.class.getClassLoader().getResourceAsStream(CONFIG_FILE);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        info(config);
+    }
 
-	}
+    // 打印配置信息
+    private static void info(Configuration config) {
+        Context context = config.getContext("mysql");
+        SqlMapGeneratorConfiguration sqlmapConfig = context.getSqlMapGeneratorConfiguration();
+        System.out.println(sqlmapConfig.getTargetProject());
+        JavaClientGeneratorConfiguration javaClientConfig = context.getJavaClientGeneratorConfiguration();
+        System.out.println(javaClientConfig.getTargetProject());
+        JavaModelGeneratorConfiguration javaModelConfig = context.getJavaModelGeneratorConfiguration();
+        System.out.println(javaModelConfig.getTargetProject());
+
+    }
 
 }
