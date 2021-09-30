f545322ec98a284f058214efd8083b2e084e9a1d
Sod-Momas <sothereer@gmail.com>
2021-09-30 12:39:39 +0800

    add init.sql

---
+++ playground-spring-boot/playground-spring-boot-multidatasource/src/init.sql
@@ -0,0 +1,88 @@
+CREATE SCHEMA multi_db_0;
+CREATE SCHEMA multi_db_1;
+CREATE SCHEMA multi_db_2;
+CREATE SCHEMA multi_db_3;
+CREATE SCHEMA multi_db_4;
+CREATE SCHEMA multi_db_5;
+CREATE SCHEMA multi_db_6;
+CREATE SCHEMA multi_db_7;
+
+USE multi_db_0;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_1;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_2;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_3;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_4;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_5;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_6;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
+
+USE multi_db_7;
+CREATE TABLE multi_tb_0(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_1(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_2(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_3(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_4(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_5(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_6(id BIGINT, remark VARCHAR(255));
+CREATE TABLE multi_tb_7(id BIGINT, remark VARCHAR(255));
