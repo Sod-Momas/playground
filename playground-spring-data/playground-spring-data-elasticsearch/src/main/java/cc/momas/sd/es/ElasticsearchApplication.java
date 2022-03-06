a3d530d4efeb51c02557f4339dfbc9cc6ffe119b
Sod-Momas <sothereer@gmail.com>
2022-03-06 21:10:42 +0800

    :tada: add playground-spring-data-elasticsearch

---
+++ playground-spring-data/playground-spring-data-elasticsearch/src/main/java/cc/momas/sd/es/ElasticsearchApplication.java
@@ -0,0 +1,15 @@
+package cc.momas.sd.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sod-Momas
+ * @since 2022/3/6
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class);
+    }
+}
