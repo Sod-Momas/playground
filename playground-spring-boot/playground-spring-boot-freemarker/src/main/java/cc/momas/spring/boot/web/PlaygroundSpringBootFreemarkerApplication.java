24fd20fc135934f9761d354369ec4faf3004a8a4
Sod-Momas <sothereer@gmail.com>
2022-04-08 09:13:31 +0800

    add module playground-spring-boot-freemarker

---
+++ playground-spring-boot/playground-spring-boot-freemarker/src/main/java/cc/momas/spring/boot/web/PlaygroundSpringBootFreemarkerApplication.java
@@ -0,0 +1,15 @@
+package cc.momas.spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sod-Momas
+ * @since 2022/4/8
+ */
+@SpringBootApplication
+public class PlaygroundSpringBootFreemarkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundSpringBootFreemarkerApplication.class, args);
+    }
+}
