dbfb7e16b41a61a65e9155b1b1b83ae650364651
Sod-Momas <sothereer@gmail.com>
2022-04-08 19:57:18 +0800

    更换依赖的类，防止抛异常

--- playground-spring-boot/playground-spring-boot-freemarker/src/main/java/cc/momas/spring/boot/web/SpringFreeMarkerService.java
+++ playground-spring-boot/playground-spring-boot-freemarker/src/main/java/cc/momas/spring/boot/web/SpringFreeMarkerService.java
@@ -5,24 +5,23 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Objects;
 
 /**
+ *
  * @author Sod-Momas
  * @since 2022/4/8
  */
-@Component
+//@Component
 public class SpringFreeMarkerService {
     @Autowired
-    private FreeMarkerConfigurationFactoryBean configuration;
+    private Configuration config;
 
     public String parse(String template, Object model) throws IOException, TemplateException {
-        final Configuration config = configuration.getObject();
-        Objects.requireNonNull(config, "freemarker configuration not found.");
+        Objects.requireNonNull(config, "freemarker configuration not found. please check current environment is NOT web environment.");
         final Template temp = config.getTemplate(template);
         StringWriter writer = new StringWriter();
         temp.process(model, writer);
