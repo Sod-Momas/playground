454d263726ba41236bc3a53d23444409d31b39b7
Sod-Momas <sothereer@gmail.com>
2022-04-18 00:54:08 +0800

    add module playground-spring-shell

---
+++ playground-spring-shell/ReadMe.md
@@ -0,0 +1,13 @@
+# playground-spring-shell
+
+Spring Shell 示例
+
+## TODO 
+
+1. 该示例的依赖版本与其他模块不一致，会导致许多不同版本的依赖进来，启动项目慢很多，需要独立出去
+2. 该项目与 playground-apache-commons-cli 项目类似，可以对比一下
+3. 该项目不支持 java9，需要适配更低版本的 java
+
+## reference
+
+- [Spring Shell入门介绍 ](https://www.cnblogs.com/nuccch/p/11067342.html)
---
+++ playground-spring-shell/src/main/java/cc/momas/spring/shell/PlaygroundSpringShellApplication.java
@@ -0,0 +1,11 @@
+package cc.momas.spring.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpringShellApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundSpringShellApplication.class, args);
+    }
+}
