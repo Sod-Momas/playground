23715be22d6ec43c83427bc956eade0a1a4c0db3
Sod-Momas <sothereer@gmail.com>
2022-04-19 00:48:38 +0800

    add module playground-spring-batch

---
+++ playground-spring-batch/ReadMe.md
@@ -0,0 +1,5 @@
+# playground-spring-batch
+
+## reference
+
+- [Spring Batch 入门教程](https://zhuanlan.zhihu.com/p/91691608)
---
+++ playground-spring-batch/src/main/java/cc/momas/spring/batch/SpringBatchApplication.java
@@ -0,0 +1,16 @@
+package cc.momas.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sod-Momas
+ * @since 2022/4/19
+ */
+
+@SpringBootApplication
+public class SpringBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+}
