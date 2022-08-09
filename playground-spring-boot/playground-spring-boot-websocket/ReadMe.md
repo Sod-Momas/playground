b069a9ab0dddc2808f5ad20dc8b31532fb2bfcf1
Sod-Momas <sothereer@gmail.com>
2022-08-10 00:13:12 +0800

    add SockJS group chat function

--- playground-spring-boot/playground-spring-boot-websocket/ReadMe.md
+++ playground-spring-boot/playground-spring-boot-websocket/ReadMe.md
@@ -8,13 +8,21 @@ websocket 示例demo
 mvn spring-boot:run
 ```
 
-启动完成之后访问 http://localhost:8080/
+启动完成之后访问 http://localhost:8080 可以进行默认页面，选择技术栈
+
+进入 http://localhost:8080/plain-text.html
 
 ![首页](docs/首页.png)
 
 启动两个页面，然后可以互相发消息
 
 ![首页](docs/聊天页.png)
+
+- - -
+
+另一个技术栈使用了 SockJS 来搭建，也是 Spring 内置的模块，访问地址为 http://localhost:8080/sockjs.html
+
+![SockJS demo](docs/sockjs-demo.jpg)
 
 ## 参考链接
 
--- playground-spring-boot/playground-spring-boot-websocket/src/main/java/cc/momas/sb/websocket/SockJSHandler.java
+++ playground-spring-boot/playground-spring-boot-websocket/src/main/java/cc/momas/sb/websocket/SockJSHandler.java
@@ -5,18 +5,62 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 /**
  * @author Sod-Momas
  * @since 2022/8/9
  */
 public class SockJSHandler extends AbstractWebSocketHandler {
     private final Logger log = LoggerFactory.getLogger(SockJSHandler.class);
+    private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("session open, id={}", session.getId());
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("session close, id={}", session.getId());
+        sessions.remove(session);
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         log.info("text message: {}", message.getPayload());
 
-        final TextMessage send = new TextMessage(session.getId() + "说：" + message.getPayload());
-        session.sendMessage(send);
+        final TextMessage msg = new TextMessage(session.getId() + "说：" + message.getPayload());
+        broadcast(msg);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        // 未成功
+        broadcast(new TextMessage("收到文件，正在处理"));
+
+        final Path tmp = Files.createTempFile("websocket-", ".tmp");
+        final ByteBuffer payload = message.getPayload();
+        payload.flip();
+        final SeekableByteChannel ch = Files.newByteChannel(tmp);
+        ch.write(payload);
+        ch.close();
+        log.info("file write to {}", tmp.toAbsolutePath());
+
+        broadcast(new TextMessage("文件处理完成，保存至 " + tmp.toAbsolutePath()));
+    }
+
+
+    private void broadcast(WebSocketMessage<?> msg) throws IOException {
+        for (WebSocketSession session : sessions) {
+            session.sendMessage(msg);
+        }
     }
 }
