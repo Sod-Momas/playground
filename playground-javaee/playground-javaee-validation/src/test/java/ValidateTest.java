a78dd5f43615c5d6f03ca305f826a706a1461320
Sod-Momas <sothereer@gmail.com>
2022-09-15 23:54:26 +0800

    fix maven error v2

--- playground-javaee/playground-javaee-validation/src/test/java/ValidateTest.java
+++ playground-javaee/playground-javaee-validation/src/test/java/ValidateTest.java
@@ -4,5 +4,5 @@ import javax.validation.Validator;
 public class ValidateTest {
 
     // 默认的验证者,用的是Hibernate
-    private static Validator validator = Validation.byDefaultProvider().configure().buildValidatorFactory().getValidator();
+//    private static Validator validator = Validation.byDefaultProvider().configure().buildValidatorFactory().getValidator();
 }
